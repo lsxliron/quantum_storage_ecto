2dbc695f3c542c5a0eade1f1b75db50168337916
lsxliron <lsxliron@noreply.com>
2025-02-25 01:58:52 -0500

    added migrations

---
+++ priv/repo/migrations/20250225044240_add_id_to_job.exs
@@ -0,0 +1,15 @@
+defmodule QuantumStorageEcto.Repo.Migrations.AddIdToJob do
+  use Ecto.Migration
+
+  def up do
+    alter table :quantum_jobs do
+      add :id, :binary, null: false
+   end
+  end
+
+  def down do
+    alter table :quantum_jobs do
+      remove :id
+    end
+  end
+end
